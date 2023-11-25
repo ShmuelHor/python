cd1c4bfa50494570312f6e056bbaa576fc76dab8
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2023-11-25 23:04:56 +0200

    תרגיל 2

--- 01_Getstarted.py
+++
@@ -1,7 +0,0 @@
-def mere(): #סתם איזה שהו תנאי
-    global  myNane # אני מגדיר שיהיה אפשר להשתמש עם המשתנה מחוץ לתנאי
-    myNane = ["shm","uel","hor"] # הוא כרגע מערך
-    global x,y,z
-    x,y,z = myNane # מפרק את המערך
-mere() #נמר התנאי
-print(x+y,z) # מדפיס
