3b0ed5333ab43ffa88a95bf7e338c70135d88dec
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2023-12-25 19:35:54 +0200

    תרגיל 4

---
+++ Exercises/functions/Exercise_04.py
@@ -0,0 +1,8 @@
+# תרגיל 4: יצירת פונקציה עם ארגומנט ברירת מחדל
+# פה אני קובע את הערך ברירת המחדל שלי
+def show_Employee(Name,Salary = 9000): 
+
+    return print(" Name:",Name ,"|","Salary:",Salary)
+
+show_Employee("Ben", 12000)
+show_Employee("Jessa")
