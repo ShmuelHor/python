589e1c0419c87acb27a2dab35953a3f449988c54
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2023-12-26 20:08:44 +0200

    תרגיל 7

---
+++ Exercises/functions/Exercise_07.py
@@ -0,0 +1,8 @@
+# תרגיל 7: הקצו שם אחר לתפקוד וקראו לו בשם החדש
+def display_student(name, age):
+    print(name, age)
+
+display_student("Emma", 26)
+
+showStudent = display_student
+showStudent("Emma", 26)
