5ebf8d2fc8f02bf6e1b623751bcb70f902168b03
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2023-12-28 21:21:14 +0200

    a

--- Exercises/Class/aaa.py
+++ Exercises/Class/aaa.py
@@ -5,8 +5,13 @@ class shmuel:
         
     def __str__(self):
         return f"{self.nam}: {self.ag}"
+    
+    def myfunc(self):
+        print("Hello my name is: " + self.nam)
+
         
 p1 = shmuel("shmuel", 20)
-
 print(p1)
 
+p1 = shmuel("John", 36)
+p1.myfunc()
