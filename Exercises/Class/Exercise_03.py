1011ba077907e860212198794e7fc4827cf28ebb
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2024-01-04 20:50:44 +0200

    תרגיל 3

---
+++ Exercises/Class/Exercise_03.py
@@ -0,0 +1,13 @@
+# תרגיל 3: צור אוטובוס כיתת ילד שיורש את כל המשתנים והשיטות של מחלקת הרכב
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+        
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
