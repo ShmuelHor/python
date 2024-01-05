762e1e43d70bcece08d864d8f55128ff917653e3
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2024-01-05 15:08:35 +0200

    תרגיל 7

---
+++ Exercises/Class/Exercise_07.py
@@ -0,0 +1,13 @@
+# Exercise 7: Check type of an object
+ 
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+print(type(School_bus))
