f6a9bd6a40f3e08449927f967883bedecf6d07ec
Yoel Horvitz <yoelhor@live.com>
2024-01-05 08:23:27 +0200

    Update Exercise_05.py

--- Exercises/Class/Exercise_05.py
+++ Exercises/Class/Exercise_05.py
@@ -1,12 +1,16 @@
 # תרגיל 5: הגדר מאפיין שחייב להיות בעל אותו ערך עבור כל מופע מחלקה (אובייקט)
 class Vehicle:
+    # Static variables are allocated memory once when the object for the class is created for the first time.
+    # Static variables are created outside of methods but inside a class
+    color = "white"
+    
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
-        
-    def aaa(self):
-        color = "white" 
+
+    # Print the content of the class
+    def Print(self): 
         return f"Color: {color} | Vehicle name: {self.name} | Speed: {self.max_speed} | Mileage: {self.mileage}"
 
 class Bus(Vehicle):
@@ -15,5 +19,5 @@ class Bus(Vehicle):
 class Car(Vehicle):
     pass
 
-x = Car("School Volvo",180,12)
-print(x.aaa())
+car = Car("School Volvo",180,12)
+print(car.Print())
