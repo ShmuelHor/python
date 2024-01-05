3988998bffdbd880907a421f23cdb2a4973598e3
Yoel Horvitz <yoelhor@live.com>
2024-01-05 08:14:58 +0200

    Update Exercise_04.py

--- Exercises/Class/Exercise_04.py
+++ Exercises/Class/Exercise_04.py
@@ -1,4 +1,4 @@
-# תרגיל 4: ירושה כיתתית
+# תרגיל 4: ירושה מחלקתית
 class Vehicle:
     def __init__(self, name, max_speed, mileage):
         self.name = name
@@ -9,9 +9,10 @@ class Vehicle:
         return f"The seating capacity of a {self.name} is {capacity} passengers"
     
 class Bus(Vehicle):
-
+    # דריסת הפוקציה עם אותם משתנים, אבל בתוספת ערך ברירת מחדל
+    # Source: https://www.w3schools.com/python/gloss_python_function_default_parameter.asp
     def seating_capacity(self, capacity=50):
-        return super().seating_capacity(capacity=50)
+        return super().seating_capacity(capacity) # אין צורך לחזור על ערך ברירת המחדל לדעתי
 
 School_bus = Bus("School Volvo", 180, 12)
 print(School_bus.seating_capacity())
