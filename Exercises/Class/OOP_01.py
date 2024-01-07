47eb9399fcf1d739e348b3ba0391f41e1568b196
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2024-01-07 20:42:33 +0200

    תרגיל 1 חדש

--- Exercises/Class/OOP_01.py
+++ Exercises/Class/OOP_01.py
@@ -1,18 +1,17 @@
 # Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.
+import math
 
 class Circle():
-    pie = 3.14
     def __init__(self,radius):
         self.radius = radius
         
     def Area_calculation(self):
-        Space = self.pie * self.radius * self.radius
-        return f"The area of the circle is: {Space}"
+        return math.pi * self.radius **2
     
     def Scope_calculation(self):
-        scope = 2 * self.pie * self.radius
-        return f"The circumference of the circle is: {scope}"
-        
+        return 2 * math.pi * self.radius
+    
 x = Circle(int(input("Please enter the radius of the circle: ")))
-print(x.Area_calculation())
-print(x.Scope_calculation())
+
+print("The area of the circle is:{:10.2f}".format(x.Area_calculation()))
+print("The circumference of the circle is:{:10.2f}".format(x.Scope_calculation()))
