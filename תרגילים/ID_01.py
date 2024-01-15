2146b5fc8e5cfb2c1f0defda6fcdf97527784ac6
ShmuelHor <150048002+ShmuelHor@users.noreply.github.com>
2024-01-15 15:29:08 +0200

    סופי

--- תרגילים/ID_01.py
+++ תרגילים/ID_01.py
@@ -6,41 +6,34 @@ class ID():
     # Override the array
     equals = [0,0,0,0,0,0,0,0]
     sum = 0
+    x = 0
+    calculator = 10
     def __init__(slaf,id):
         slaf.id = id
 
     def examination(slaf):
 
-        for x in range(0,8):
-
+        for slaf.x in range(0,8):
             # I am multiplier the format by the id
-            slaf.equals[x] += slaf.id[x] * slaf.base[x]
+            slaf.equals[slaf.x] += slaf.id[slaf.x] * slaf.base[slaf.x]
 
             # If the sum contains two numbers 
-            if (slaf.equals[x] > 9):
+            if (slaf.equals[slaf.x] > 9):
 
                 # then I add them together For example 12 = 1+2 =3 
-                slaf.equals[x]= sum(map(int, str(slaf.equals[x])))
+                slaf.equals[slaf.x]= sum(map(int, str(slaf.equals[slaf.x])))
             # I add up all the numbers in the array
-            slaf.sum += slaf.equals[x]
-            # Here I add the check digit for the ID
+            slaf.sum += slaf.equals[slaf.x]
+            
+        # Here I add the check digit for the ID
         if (slaf.sum % 10) != 0:
-            if slaf.sum < 20:
-                slaf.id.append(20 - slaf.sum)
-            elif 20 < slaf.sum < 30:
-                slaf.id.append(30 - slaf.sum)
-            elif 30 < slaf.sum < 40:
-                slaf.id.append(40 - slaf.sum)
-            elif 40 < slaf.sum < 50:
-                slaf.id.append(50 - slaf.sum)
-            elif 50 < slaf.sum < 60:
-                slaf.id.append(60 - slaf.sum)
-            elif 60 < slaf.sum < 70:
-                slaf.id.append(70 - slaf.sum)
-            elif 70 < slaf.sum < 80:
-                slaf.id.append(80 - slaf.sum)
-            else:
-                slaf.equals.append(0)
+            for e in range(9):
+                slaf.calculator += 10
+
+                if (slaf.calculator-10) < slaf.sum < slaf.calculator:
+                    slaf.id.append(slaf.calculator - slaf.sum)
+        else:
+            slaf.id.append(0)          
         return slaf.id
 
 my_randoms=[]
